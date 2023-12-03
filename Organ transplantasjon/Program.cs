cfadd228bb83e4ca6faa53b4dd63107842a137b4
PalSvendsen <pal@getacademy.no>
2023-12-03 11:09:36 +0100

    Add project files.

---
+++ Organ transplantasjon/Program.cs
@@ -0,0 +1,52 @@
+﻿using Organ_transplantasjon;
+
+namespace Organ
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool playAgain = true;
+            var Kåre = new pasienter("Kåre", 34);
+            var Bjarne = new pasienter("Bjarne", 37);
+            while (playAgain)
+            {
+            Random num = new Random();
+            int chances = num.Next(1,101);
+            int survival = num.Next(1,101);
+
+            Console.WriteLine($"overlevelse med {chances}");
+            Console.WriteLine($"Ikke overlevelse med {survival}");
+
+            if (chances >= survival)
+            {
+                Console.WriteLine("Bjarne overlevde transplantasjonen");
+            }
+            else
+            {
+                Console.WriteLine("Bjarne overlevde ikke");
+            }
+
+            Console.WriteLine("Trykk ja/nei for å prøve igjen!");
+            string answer = Console.ReadLine().ToUpper();
+
+            if (answer == "JA")
+            {
+                Console.WriteLine("La oss prøve igjen!");
+            }
+            else if (answer == "NEI")
+            {
+                Console.WriteLine("Vi sees!");
+                playAgain = false;
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Feil inntast!");
+                playAgain = false;
+                break;
+            }
+        }
+        }
+    }
+}
---
+++ Organ transplantasjon/pasienter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organ_transplantasjon
+{
+    public class pasienter
+    {
+        public string Name;
+        public int Age;
+
+        public pasienter(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
